d64aca9f2cb52bc98a2eaf59d2eb707296148986
Mathieu GARNIER <mgarnier11@gmail.com>
2023-08-28 14:42:02 +0200

    feat: added more tests to videoTrack, added debuger for jest

--- src/lib/videoTrack/videoTrack.test.ts
+++ src/lib/videoTrack/videoTrack.test.ts
@@ -1,41 +1,26 @@
 import { registerComponents } from "../components/component/component";
 import { registerEffects } from "../effects/effect/effect";
 import * as utils from "../utils/utils";
-import * as fs from "fs";
+import fs from "fs";
+import threads from "worker_threads";
+import url from "url";
+import path from "path";
+import canvas from "@mgarnier11/my-canvas";
+import { EventEmitter } from "events";
 import { VideoTrack } from "./videoTrack";
+
+jest.mock("fs");
+jest.mock("worker_threads");
+jest.mock("url");
+jest.mock("path");
+jest.mock("@mgarnier11/my-canvas", () => ({
+  Canvas: jest.fn(),
+}));
 
 describe("VideoTrack", () => {
   beforeAll(async () => {
     await registerComponents();
     await registerEffects();
-
-    jest.mock("fs", () => {
-      const originalFs = jest.requireActual("fs");
-      return {
-        ...originalFs,
-        writeFileSync: jest.fn(),
-      };
-    });
-
-    jest.mock("worker_threads", () => {
-      const originalWorkerThreads = jest.requireActual("worker_threads");
-      return {
-        ...originalWorkerThreads,
-        Worker: class {
-          on = jest.fn();
-        },
-      };
-    });
-
-    jest.mock("path", () => {
-      const originalPath = jest.requireActual("path");
-
-      return {
-        ...originalPath,
-
-        dirname: jest.fn(() => "test"),
-      };
-    });
   });
 
   describe("VideoTrack.Builder", () => {
@@ -195,56 +180,141 @@ describe("VideoTrack", () => {
         jest.spyOn(videoTrack, "generateFramesSync").mockImplementation(() => {
           throw new Error();
         });
+        jest.spyOn(console, "error").mockImplementation(() => {});
 
         const result = await videoTrack.generateFrames("test", 0, null);
 
         expect(result).toBe(false);
+        expect(console.error).toHaveBeenCalled();
       });
     });
 
-    // describe("generateFramesSync", () => {
-    //   it("should call drawFrame", async () => {
-    //     const videoTrack: any = new VideoTrack.Builder().withDuration(10).withFramerate(1).build();
+    describe("generateFramesSync", () => {
+      it("should call drawFrame", async () => {
+        fs.writeFileSync = jest.fn();
 
-    //     const drawFrameSpy = jest.spyOn(videoTrack, "drawFrame").mockImplementation(() => {});
-    //     const progressCallback = jest.fn();
+        const videoTrack: any = new VideoTrack.Builder().withDuration(10).withFramerate(1).build();
 
-    //     const result = await videoTrack.generateFramesSync("test", progressCallback);
+        const drawFrameSpy = jest.spyOn(videoTrack, "drawFrame").mockImplementation(() => {});
+        const progressCallback = jest.fn();
 
-    //     expect(drawFrameSpy).toHaveBeenCalledTimes(10);
-    //     expect(progressCallback).toHaveBeenCalledTimes(10);
-    //     expect(fs.writeFileSync).toHaveBeenCalledTimes(10);
-    //     expect(result).toBe(true);
-    //   });
-    // });
+        const result = await videoTrack.generateFramesSync("test", progressCallback);
 
-    // fdescribe("generateFramesAsync", () => {
-    //   it("should generate new workers", async () => {
-    //     const videoTrack: any = new VideoTrack.Builder().withDuration(10).withFramerate(1).build();
-    //     const json = JSON.stringify(videoTrack.toJSON());
+        expect(drawFrameSpy).toHaveBeenCalledTimes(10);
+        expect(progressCallback).toHaveBeenCalledTimes(10);
+        expect(fs.writeFileSync).toHaveBeenCalledTimes(10);
+        expect(result).toBe(true);
+      });
+    });
 
-    //     const sharedBuffer = new SharedArrayBuffer(Buffer.byteLength(json));
-    //     new Uint8Array(sharedBuffer).set(Buffer.from(json));
+    describe("generateFramesAsync", () => {
+      const eventEmitter = new EventEmitter();
+      const constructorMock = jest.fn();
 
-    //     const progressCallback = jest.fn();
-    //     const fileURLToPathSpy = jest.spyOn(require("url"), "fileURLToPath").mockImplementation(() => "test");
+      beforeAll(() => {
+        // @ts-ignore
+        threads.Worker = class {
+          constructor(...args: any[]) {
+            constructorMock(...args);
+            return eventEmitter;
+          }
+        };
+      });
 
-    //     const result = videoTrack.generateFramesAsync("test", 1, progressCallback);
+      it("should generate new workers", async () => {
+        path.join = jest.fn(() => "test/videoTrackWorker.js");
 
-    //     // expect(Worker).toHaveBeenCalledTimes(4);
-    //     expect(Worker).toHaveBeenCalledWith([
-    //       "test/videoTrackWorker.js",
-    //       {
-    //         workerData: {
-    //           sharedBuffer,
-    //           outputPath: "test",
-    //           startFrame: 0,
-    //           endFrame: 10,
-    //           workerNb: 0,
-    //         },
-    //       },
-    //     ]);
-    //   });
-    // });
+        const videoTrack: any = new VideoTrack.Builder().withDuration(10).withFramerate(10).build();
+
+        const json = JSON.stringify(videoTrack.toJSON());
+        const sharedBuffer = new SharedArrayBuffer(Buffer.byteLength(json));
+        new Uint8Array(sharedBuffer).set(Buffer.from(json));
+
+        const progressCallback = jest.fn();
+
+        const resultPromise = videoTrack.generateFramesAsync("test", 1, progressCallback);
+
+        const expectedCallbackCalls: number[][] = [];
+
+        for (let i = 0; i < 100; i++) {
+          eventEmitter.emit("message", { type: "new-frame", data: { frame: i } });
+          expectedCallbackCalls.push([i + 1, 100]);
+        }
+        eventEmitter.emit("exit");
+
+        const result = await resultPromise;
+
+        expect(constructorMock).toHaveBeenCalledTimes(1);
+        expect(constructorMock.mock.calls).toEqual([
+          [
+            "test/videoTrackWorker.js",
+            {
+              workerData: {
+                sharedBuffer,
+                outDir: "test",
+                startFrame: 0,
+                endFrame: 100,
+                workerNb: 0,
+              },
+            },
+          ],
+        ]);
+        expect(progressCallback).toHaveBeenCalledTimes(100);
+        expect(progressCallback.mock.calls).toEqual(expectedCallbackCalls);
+        expect(result).toBe(true);
+      });
+
+      it("should throw an error when it is caught", async () => {
+        const videoTrack: any = new VideoTrack.Builder().withDuration(10).withFramerate(10).build();
+
+        const json = JSON.stringify(videoTrack.toJSON());
+        const sharedBuffer = new SharedArrayBuffer(Buffer.byteLength(json));
+        new Uint8Array(sharedBuffer).set(Buffer.from(json));
+
+        const progressCallback = jest.fn();
+
+        const resultPromise = videoTrack.generateFramesAsync("test", 1, progressCallback);
+
+        eventEmitter.emit("error", new Error());
+
+        expect(resultPromise).rejects.toThrowError();
+      });
+    });
+
+    describe("drawFrame", () => {
+      it("should return the correct frame", () => {
+        const drawFn = jest.fn();
+        const canvasConstructor = jest.fn();
+        const contextFillRect = jest.fn();
+        const canvasGetContext = jest
+          .fn()
+          .mockReturnValue({ fillRect: contextFillRect, canvas: { width: 1920, height: 1080 } });
+
+        // @ts-ignore
+        canvas.Canvas = class {
+          constructor(...args: any[]) {
+            canvasConstructor(...args);
+          }
+          getContext = canvasGetContext;
+          toBuffer = jest.fn(() => Buffer.from("test"));
+        };
+
+        const videoTrack: any = new VideoTrack.Builder()
+          .withDuration(10)
+          .withFramerate(60)
+          .withHeight(1080)
+          .withWidth(1920)
+          .withComponents([{ draw: drawFn }, { draw: drawFn }, { draw: drawFn }] as any)
+          .build();
+
+        const frame = videoTrack.drawFrame(0);
+
+        expect(canvasConstructor).toHaveBeenCalledWith(1920, 1080);
+        expect(canvasGetContext).toHaveBeenCalledWith("2d");
+        expect(contextFillRect).toHaveBeenCalledWith(0, 0, 1920, 1080);
+        expect(drawFn).toHaveBeenCalledTimes(3);
+        expect(frame).toBeInstanceOf(Buffer);
+      });
+    });
   });
 });
